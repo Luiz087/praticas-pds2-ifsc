550e1a0be3c04de049d69dbcdd52274c98f26aa1
Luiz Fernando <luiz.s08@aluno.ifsc.edu.br>
2023-08-15 09:57:42 -0300

    listar bd

--- src/main/java/controle/CarroDAO.java
+++ src/main/java/controle/CarroDAO.java
@@ -3,50 +3,65 @@ package controle;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import com.mysql.cj.xdevapi.PreparableStatement;
 
 import modelo.Carro;
 
 public class CarroDAO {
-	
-	
+
 	/**
 	 * Metodo que recebe um carro
 	 * 
 	 * @param c
 	 * @return
 	 */
-	public boolean inserir(Carro p) {
-		
-		// Instanciar a classe
+
+	public ArrayList<Carro> listar() {
 		Conexao c = Conexao.getInstancia();
-		
-		//Abrir a conexão com o banco
+
+		// Abrir a conexão com o banco
 		Connection con = c.conectar();
 		
-		String query = "INSERT INTO carro (id_carro, modelo_carro) VALUES (?, ?)";
+		// Criando um obj de lista para guardar o que o banco trouxe
+		ArrayList<Carro> carro = new ArrayList();
+
+		String query = "SELECT * FROM carros";
 		
+		return carro;
+	}
+
+	public boolean inserir(Carro p) {
+
+		// Instanciar a classe
+		Conexao c = Conexao.getInstancia();
+
+		// Abrir a conexão com o banco
+		Connection con = c.conectar();
+
+		String query = "INSERT INTO carro (id_carro, modelo_carro) VALUES (?, ?)";
+
 		try {
 			PreparedStatement ps = con.prepareStatement(query);
-			
+
 			// Seta os parâmetros
 			ps.setInt(1, p.getIdCarro());
 			ps.setString(2, p.getModeloCarro());
-			
+
 			// Consolidar a execução do comando no banco
 			ps.executeUpdate();
-			
+
 			// Fecha a conexão
 			c.fecharConexao();
-			
+
 			return true;
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return false;
 	}
 
--- src/main/java/visao/telaTeste.java
+++ src/main/java/visao/telaTeste.java
@@ -5,10 +5,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class telaTeste extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textIdCarro;
+	private JTextField textModelo;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -31,11 +39,51 @@ public class telaTeste extends JFrame {
 	 */
 	public telaTeste() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1920, 1080);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Id Carro:");
+		lblNewLabel.setBounds(10, 22, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		textIdCarro = new JTextField();
+		textIdCarro.setBounds(66, 19, 86, 20);
+		contentPane.add(textIdCarro);
+		textIdCarro.setColumns(10);
+		
+		JLabel lblModelo = new JLabel("Modelo:");
+		lblModelo.setBounds(10, 57, 46, 14);
+		contentPane.add(lblModelo);
+		
+		textModelo = new JTextField();
+		textModelo.setColumns(10);
+		textModelo.setBounds(66, 54, 86, 20);
+		contentPane.add(textModelo);
+		
+		JButton btnNewButton = new JButton("INSERIR");
+		btnNewButton.setBounds(10, 85, 142, 23);
+		contentPane.add(btnNewButton);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Id", "Modelo"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				Integer.class, String.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		table.setBounds(10, 130, 600, 237);
+		contentPane.add(table);
 	}
-
 }
